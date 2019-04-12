481719551522b588457bc443d2fd11f2c2f05e8f
Fabian Schierok <fabian.schierok@udo.edu>
2019-04-12 19:05:42 +0200

    daten eingetragen und formeln eingetragen

--- plot.py
+++ plot.py
@@ -1,10 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.constants import g
 
 
-def flughöhe(x, t):
-    pass
+def flugwinkel(x, t):
+    return np.arctan(t**2*g/(2*x))
+
+
+def startGeschwindigkeit(x, alpha):
+    return np.sqrt(x*g/(np.sin(2*alpha)))
+
+
+def flughöhe(v, alpha, t):
+    return v*np.sin(alpha)*t/2 - g*t**2/8
 
 
 def flugparabel():
     pass
+
+
+data = np.genfromtxt("data/2019-04-12.txt", unpack=True)
+
+alpha = flugwinkel(data[2], data[3])
+v0 = startGeschwindigkeit(data[2], alpha)
+ymax = flughöhe(v0, alpha, data[3])
